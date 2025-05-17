20296c95aec996d0c328355145fe7816a28e1c6e
kingsand <clinvil2@gmail.com>
2025-05-17 15:25:52 -0700

    minor fixes to run_ensemble_knn_batching

--- final/run_ensemble_knn_batching.py
+++ final/run_ensemble_knn_batching.py
@@ -27,7 +27,7 @@ print("ENSEMBLE VOTING (K-NN):")
 # knn_bagging_model = build_ensemble_bagging_knn(167, 23, 'distance', 2)    # val:60 test:80 f1:82.76
 # knn_bagging_model = build_ensemble_bagging_knn(130, 213, 'uniform', 2)    # val:72 test:84 f1:87.5
 # knn_bagging_model = build_ensemble_bagging_knn(149, 213, 'uniform', 2)    # val:72 test:84 f1:87.5
-76
+
 # knn_bagging_model = build_ensemble_bagging_knn(10, 32, 'uniform', 2)      # val:64 test:76 f1:79.31
 # knn_bagging_model = build_ensemble_bagging_knn(20, 40, 'uniform', 2)      # val:74 test:78 f1:81.97
 # knn_bagging_model = build_ensemble_bagging_knn(50, 48, 'uniform', 2)      # val:74 test:80 f1:83.33
@@ -35,8 +35,8 @@ print("ENSEMBLE VOTING (K-NN):")
 # knn_bagging_model = build_ensemble_bagging_knn(150, 64, 'uniform', 2)     # val:72 test:80 f1:83.87
 # knn_bagging_model = build_ensemble_bagging_knn(200, 72, 'uniform', 2)     # val:70 test:82 f1:85.71
 # knn_bagging_model = build_ensemble_bagging_knn(250, 80, 'uniform', 2)     # val:72 test:82 f1:85.71
-# knn_bagging_model = build_ensemble_bagging_knn(300,  70, 'uniform', 2)    # val:70 test:82 f1:85.71
-# knn_bagging_model = build_ensemble_bagging_knn(350,  85, 'distance', 1)   # val:70 test:74 f1:79.37
+# knn_bagging_model = build_ensemble_bagging_knn(300, 70, 'uniform', 2)     # val:70 test:82 f1:85.71
+# knn_bagging_model = build_ensemble_bagging_knn(350, 85, 'distance', 1)    # val:70 test:74 f1:79.37
 
 # knn_bagging_model = build_ensemble_bagging_knn(100, 213, 'uniform', 2)    # val:72 test:82 f1:86.15
 # knn_bagging_model = build_ensemble_bagging_knn(130, 213, 'uniform', 1)    # val:70 test:80 f1:84.38
